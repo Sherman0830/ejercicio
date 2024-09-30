a4ab26fb0fad6d2b7a42f6bdbc6f73d21e91c61a
Andrés Cárdenas <estebancardenas777@gmail.com>
2024-09-30 11:13:33 -0500

    hola

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*
+        andres esteba cardenas montañez
+        andres.cardenas-m@uniminuto.edu.co
+        Zipaquira - Villa Maria
+        Gimnasio
+
+        */
+
+    }
+}
